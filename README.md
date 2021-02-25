bddf22e2ee0ab66fa297374a7b370d575ff9fe04
Daniel Morales <daniel85mg@gmail.com>
2021-02-25 14:22:59 -0300

    inital import

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple project to demonstrate the use of Laravel 8 Jetstream components (Livewire)
---
+++ database/factories/CourseFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Course;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CourseFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Course::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->sentence,
+            'slug' => $this->faker->slug,
+            'image' => $this->faker->imageUrl(1280, 720),
+            'description' => $this->faker->text(800),
+            'user_id' => \App\Models\User::all()->random(),
+            'category_id' => \App\Models\Category::all()->random(),
+        ];
+    }
+}
